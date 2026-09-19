82ad51fe8f5172ece28d644913001d465a1c95c2
Bader <bader.almsalam2@gmail.com>
2026-09-19 20:26:13 +0300

    Connect to Firebase Realtime Database

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 window.APP_CONFIG = {
   // رابط Firebase Realtime Database، مثل: https://kun-maher-default-rtdb.firebaseio.com
   // إذا تُرك فارغًا يشتغل الموقع في "وضع تجريبي" على الجهاز نفسه فقط.
-  dbUrl: '',
+  dbUrl: 'https://sia-dismissal-default-rtdb.europe-west1.firebasedatabase.app',
 
   schoolName: 'أكاديمية سلطان الدولية',
   appName: 'نداء الانصراف',
